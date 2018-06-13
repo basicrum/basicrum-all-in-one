b980e42e34ce92c75b18a0261fe3689f749ceb27
Tsvetan Stoychev <ceckoslab@gmail.com>
2018-06-13 08:52:24 +0200

    WIP: Added rest timing decompression lib and waterfall renderer lib.

---
+++ src/BasicRum/WaterfallSvgRenderer.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace App\BasicRum;
+
+class WaterfallSvgRenderer
+{
+
+    private $maxLength = 20000;
+
+    private $_rowEvenBackground = '#ccc';
+    private $_rowOddBackground = '#fff';
+
+    private $_lineHeight = 18;
+
+    /**
+     * To HTML
+     *
+     * @param array $navigationTimings
+     * @return string
+     */
+    public function render($navigationTimings)
+    {
+        $linesCount = count($navigationTimings['restiming']);
+
+        $waterfallHeight = $linesCount * $this->_lineHeight;
+
+        $output = '<svg class="water-fall-chart" height="' . $waterfallHeight . '">';
+
+        $output .= $this->renderTimeLines($waterfallHeight, $navigationTimings);
+
+        $output .= $this->renderWaterfall($navigationTimings['restiming']);
+
+        $output .= '</svg>';
+
+        return $output;
+    }
+
+
+    /**
+     * Render tree (recursive function)
+     *
+     * @param array $data
+     * @return string
+     */
+    public function renderWaterfall(array $data)
+    {
+        $output = '<g class="rows-holder" height="700">';
+
+        foreach ($data as $key => $resData) {
+            $zebraClass = 'even';
+
+            if ($key % 2 > 0) {
+                $zebraClass = 'odd';
+            }
+
+            $urlParts = explode('/', $resData['url']);
+            $url = $urlParts[count($urlParts) - 1];
+
+            $output .= '<a class="row-item" tabindex="0" xlink:href="javascript:void(0)" transform="translate(0, 0)">';
+            $output .= '<clipPath id="titleFullClipPath"><rect height="100%" width="100%"></rect></clipPath>';
+            $output .= '<rect class="' . $zebraClass . '" height="' . $this->_lineHeight . '" width="100%" x="0" y="' . $this->_lineHeight * $key .'"></rect>';
+            $output .= '<svg class="flex-scale-waterfall" width="65%" x="35%">';
+            $output .= '<g class="row row-flex">';
+            $output .= '<rect height="' . $this->_lineHeight . '" width="100%" x="0" y="' . $this->_lineHeight * $key .'" style="opacity: 0;"></rect>';
+
+            $output .= '<svg width="' . $this->calculateEntryWidth($resData) . '" x="' . $this->calculateEntryStart($resData) . '">';
+
+            $rowBackground = $key % 2 == 0 ? $this->_rowEvenBackground : $this->_rowOddBackground;
+
+            $output .= $this->renderResEntry($resData, $key);
+
+            $output .= '</svg>';
+
+            $output .= '</g>';
+            $output .= '</svg>';
+
+
+            $output .= '<svg class="left-fixed-holder" width="35%" x="0">';
+            $output .= '<clipPath id="titleClipPath"><rect height="100%" width="100%"></rect></clipPath>';
+
+            $output .= '<g class="row row-fixed">';
+
+            $output .= '<text x="36.343994140625" y="' . (17 + ($key * $this->_lineHeight)) . '" style="text-anchor: end;">' . ($key + 1)  . '</text>';
+
+            $output .= '<text x="40.343994140625" y="' . (17 + ($key * $this->_lineHeight)) . '">' . $resData['url'] . '<title text="' . $url . '"></title></text>';
+
+            $output .= '</g>';
+
+            $output .= '</svg>';
+
+            $output .= '</a>';
+        }
+
+        $output .= '</g>';
+
+        return $output;
+    }
+
+    /**
+     * Render css progress bar
+     *
+     * @param array $resData
+     * @param string $rowBackground
+     * @param int $resourceNumber
+     *
+     * @return string
+     */
+    protected function renderResEntry($resData, $resourceNumber)
+    {
+        $renderData = array();
+
+        $renderData['blocking'] = array(
+            'color' => '#999da3',
+            'duration' => ($resData['redirectStart'] > $resData['startTime']) ?
+                    $resData['redirectStart'] - $resData['startTime'] : 0
+        );
+
+        $renderData['redirect'] = array(
+            'color' => '#ffd942',
+            'duration' => $resData['redirectEnd'] - $resData['redirectStart']
+        );
+
+        $renderData['appCache'] = array(
+            'color' => '#6cb500',
+            'duration' => ($resData['domainLookupStart'] > $resData['fetchStart']) ?
+                    $resData['domainLookupStart'] - $resData['fetchStart'] : 0
+        );
+
+        $renderData['dns'] = array(
+            'color' => '#159588',
+            'duration' => $resData['domainLookupEnd'] - $resData['domainLookupStart']
+        );
+
+        $renderData['connect'] = array(
+            'color' => '#fd9727',
+            'duration' => ($resData['secureConnectionStart'] > $resData['connectStart']) ?
+                    $resData['secureConnectionStart'] - $resData['connectStart'] : $resData['connectEnd'] - $resData['connectStart']
+        );
+
+        $renderData['secure_connect'] = array(
+            'color' => '#c141cd',
+            'duration' => ($resData['secureConnectionStart'] > $resData['connectStart']) ?
+                    $resData['connectEnd'] - $resData['connectStart'] : 0
+        );
+
+        if (
+            $renderData['blocking']['duration'] === 0
+            && $renderData['redirect']['duration'] === 0
+            && $renderData['dns']['duration'] === 0
+            && $renderData['connect']['duration'] === 0
+            && $renderData['secure_connect']['duration'] === 0
+        ) {
+            if ($resData['requestStart'] > $resData['fetchStart']) {
+                $renderData['blocking']['duration'] = $resData['requestStart'] - $resData['fetchStart'];
+            }
+        }
+
+        $renderData['request'] = array(
+            'color' => '#1ec659',
+            'duration' => ($resData['responseStart'] > $resData['requestStart']) ?
+                    $resData['responseStart'] - $resData['requestStart'] : 0
+        );
+
+        $renderData['response'] = array(
+            'color' => '#1eaaf1',
+            'duration' => ($resData['responseEnd'] > $resData['responseStart'] && $resData['responseStart'] > 0) ?
+                    $resData['responseEnd'] - $resData['responseStart'] : $resData['responseEnd'] - $resData['startTime']
+        );
+
+        $output = '<g class="rect-holder">';
+
+        $entryLeftDistance = 0;
+
+        foreach ($renderData as $key => $entry) {
+            if ($entry['duration'] != 0 && $resData['duration'] != 0) {
+                $entryWidth = ($entry['duration'] / $resData['duration']) * 100;
+            } else {
+                /**
+                 * The case when duration is exactly 0
+                 *
+                 */
+                $entryWidth = 0.2;
+            }
+
+            if ($entryWidth > 0) {
+                $output .= '<g><rect fill="' . $entry['color'] . '" height="' . $this->_lineHeight . '" x="' . $entryLeftDistance . '%" y="' . $resourceNumber * $this->_lineHeight . '" width="' . $entryWidth . '%"></rect></g>';
+            }
+
+            $entryLeftDistance += $entryWidth;
+
+        }
+
+        $output .= '</g>';
+
+        return $output;
+    }
+
+    /**
+     * @param array $resData
+     *
+     * @return string
+     */
+    private function calculateEntryStart(array $resData)
+    {
+        $width = ($resData['startTime'] / $this->maxLength) * 100;
+
+        return $width . '%';
+    }
+
+    /**
+     * @param array $resData
+     * @return string
+     */
+    private function calculateEntryWidth(array $resData)
+    {
+        $duration = $resData['duration'];
+
+        $width = ($duration / $this->maxLength) * 100;
+
+        return $width . '%';
+    }
+
+    /**
+     * @param int $height
+     * @param array $navigationTimings
+     *
+     * @return string
+     */
+    private function renderTimeLines($height, $navigationTimings)
+    {
+        $firstPaint = $navigationTimings['nt_first_paint'] - $navigationTimings['nt_nav_st'];
+        $firstByte  = $navigationTimings['nt_res_st'] - $navigationTimings['nt_nav_st'];
+
+        $lines = '';
+
+        $lineHorizontalPosition = 0;
+
+        while ($lineHorizontalPosition <= $this->maxLength) {
+            $widthPercent = ($lineHorizontalPosition / $this->maxLength) * 100;
+
+            $strokeColor = '#ccc';
+
+            $opacity = '';
+
+            if ($lineHorizontalPosition % 1000 == 0) {
+                $strokeColor = '#0cc';
+            }
+
+            if ($lineHorizontalPosition % 1000 != 0) {
+                $opacity = 'opacity: 0.75;';
+            }
+
+            $lines .= '<line style="stroke: ' . $strokeColor . '; stroke-width: 1; ' . $opacity . '" x1="' . $widthPercent . '%" x2="' . $widthPercent . '%" y1="0" y2="' . $height . '"></line>';
+
+
+            if ($lineHorizontalPosition % 1000 == 0) {
+                $second = $lineHorizontalPosition / 1000;
+                $lines .=  '<text style="color: #606c71; font-weight: bold;" x="'. $widthPercent . '%" y="-3">' . $second . 's</text>';
+            }
+
+            $lineHorizontalPosition += 200;
+        }
+
+        if ($firstPaint > 0) {
+            $widthPercent = ($firstPaint / $this->maxLength) * 100;
+            $lines .= '<line style="stroke: #c141cd; stroke-width: 2;" x1="' . $widthPercent . '%" x2="' . $widthPercent . '%" y1="-25" y2="' . $height . '"></line>';
+            $lines .=  '<text style="color: #606c71; font-weight: bold;" x="'. ($widthPercent - 2) . '%" y="-28">First Paint (' . $firstPaint . ' ms)</text>';
+
+        }
+
+        if ($firstByte > 0) {
+            $widthPercent = ($firstByte / $this->maxLength) * 100;
+            $lines .= '<line style="stroke: #1ec659; stroke-width: 2;" x1="' . $widthPercent . '%" x2="' . $widthPercent . '%" y1="-25" y2="' . $height . '"></line>';
+            $lines .=  '<text style="color: #606c71; font-weight: bold;" x="'. ($widthPercent - 2) . '%" y="-28">First Byte (' . $firstByte . ' ms)</text>';
+
+        }
+
+        $output = '<g><svg width="65%" x="35%"><g>' . $lines .'</g></svg></g>';
+
+        return $output;
+    }
+
+}
