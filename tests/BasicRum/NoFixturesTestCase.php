516266503bf4af282051597f4b8339e0f1e98d21
Tsvetan Stoychev <ceckoslab@gmail.com>
2019-06-27 14:42:38 +0200

    #27 - We truncate all tables and reset primary key increment at the beginning of each not fixtured test.

--- tests/BasicRum/NoFixturesTestCase.php
+++ tests/BasicRum/NoFixturesTestCase.php
@@ -14,6 +14,54 @@ class NoFixturesTestCase extends KernelTestCase
         $cacheCommand->clearCache();
 
         static::bootKernel();
+
+        $this->truncateEntities($this->getAllEntityClasses());
+    }
+
+    // Idea from https://symfonycasts.com/screencast/phpunit/control-database
+
+    private function getEntityManager()
+    {
+        return self::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    /**
+     * @return array
+     */
+    private function getAllEntityClasses() : array
+    {
+        $classes = [];
+
+        $metas = $this->getEntityManager()->getMetadataFactory()->getAllMetadata();
+
+        foreach ($metas as $meta) {
+            $classes[] = $meta->getName();
+        }
+
+        return $classes;
+    }
+
+    /**
+     * @param array $entities
+     */
+    private function truncateEntities(array $entities)
+    {
+        $connection = $this->getEntityManager()->getConnection();
+        $databasePlatform = $connection->getDatabasePlatform();
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->query('SET FOREIGN_KEY_CHECKS=0');
+        }
+        foreach ($entities as $entity) {
+            $query = $databasePlatform->getTruncateTableSQL(
+                $this->getEntityManager()->getClassMetadata($entity)->getTableName()
+            );
+            $connection->executeUpdate($query);
+        }
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->query('SET FOREIGN_KEY_CHECKS=1');
+        }
     }
 
 }
