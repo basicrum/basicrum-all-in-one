22a57a099cc15fea7cd9355000f2957e23d68aa2
CodeRaiser <code.geeks1@gmail.com>
2020-03-04 19:18:22 +0200

    Fix: For migration scripts add the changes needed only for user table

--- src/Migrations/Version20200222114131.php
+++ src/Migrations/Version20200222114131.php
@@ -23,11 +23,6 @@ final class Version20200222114131 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, fname VARCHAR(255) NOT NULL, lname VARCHAR(255) NOT NULL, restore_password VARCHAR(255) NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE feedback');
-        $this->addSql('ALTER TABLE navigation_timings_user_agents CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
-        $this->addSql('ALTER TABLE navigation_timings CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
-        $this->addSql('ALTER TABLE navigation_timings_urls CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
-        $this->addSql('ALTER TABLE visits_overview CHANGE guid guid CHAR(128) DEFAULT NULL, CHANGE last_page_view_id last_page_view_id INT UNSIGNED DEFAULT NULL, CHANGE visit_duration visit_duration INT UNSIGNED DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -35,11 +30,6 @@ final class Version20200222114131 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE feedback (id INT UNSIGNED AUTO_INCREMENT NOT NULL, message TEXT NOT NULL COLLATE utf8mb4_unicode_ci, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE user');
-        $this->addSql('ALTER TABLE navigation_timings CHANGE created_at created_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
-        $this->addSql('ALTER TABLE navigation_timings_urls CHANGE created_at created_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
-        $this->addSql('ALTER TABLE navigation_timings_user_agents CHANGE created_at created_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
-        $this->addSql('ALTER TABLE visits_overview CHANGE guid guid CHAR(128) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE last_page_view_id last_page_view_id INT UNSIGNED DEFAULT NULL, CHANGE visit_duration visit_duration INT UNSIGNED DEFAULT NULL');
     }
 }
