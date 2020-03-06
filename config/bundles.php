d5504fe9d699c8b504efc4fb9f4a278ca8219388
CodeRaiser <code.geeks1@gmail.com>
2020-03-06 14:50:57 +0200

    Get rid of DoctrineFixturesBundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
