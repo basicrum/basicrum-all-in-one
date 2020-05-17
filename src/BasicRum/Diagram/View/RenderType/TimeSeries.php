38b212ccfbfd6526a1a7e94282e6199d950ac281
Ivan Nikolov <63069313+NikolovI@users.noreply.github.com>
2020-05-17 11:59:45 +0300

    Initial version of the release annotation with dummy data (#118)

--- src/BasicRum/Diagram/View/RenderType/TimeSeries.php
+++ src/BasicRum/Diagram/View/RenderType/TimeSeries.php
@@ -39,6 +39,34 @@ class TimeSeries
             $data['layout']['yaxis']['rangemode'] = 'tozero';
         }
 
+        // adding line shape to mark the precise date of the release
+        $data['layout']['shapes'][0]['type'] = 'line';
+        $data['layout']['shapes'][0]['layer'] = 'above';
+
+        $data['layout']['shapes'][0]['x0'] = '2019-07-21';
+        $data['layout']['shapes'][0]['y0'] = 0;
+        $data['layout']['shapes'][0]['x1'] = '2019-07-21';
+        $data['layout']['shapes'][0]['y1'] = 497;
+        //use y1 = 1 and yref = paper in order to have the annotation on top
+        //$data['layout']['shapes'][0]['yref'] = 'paper';
+        $data['layout']['shapes'][0]['line']['dash'] = 'dash';
+        $data['layout']['shapes'][0]['line']['color'] = '#000';
+        $data['layout']['shapes'][0]['line']['width'] = 3;
+
+        //adding the annotation right next to the marker shape
+        $data['layout']['annotations'][0]['visible'] = false;
+        $data['layout']['annotations'][0]['x'] = '2019-07-21';
+        //use y = 1 and yref = paper in order to have the annotation on top
+        $data['layout']['annotations'][0]['y'] = 497;
+        //$data['layout']['annotations'][0]['yref'] = 'paper';
+        $data['layout']['annotations'][0]['text'] = 'Release v1.1';
+        $data['layout']['annotations'][0]['showarrow'] = true;
+        $data['layout']['annotations'][0]['ax'] = 0;
+        $data['layout']['annotations'][0]['font']['size'] = '14';
+        $data['layout']['annotations'][0]['font']['color'] = '#000';
+        //annotation must be on some of the graphs in order to have clicktoshow working
+        $data['layout']['annotations'][0]['clicktoshow'] = 'onoff';
+
         foreach ($samples as $key => $d) {
             $data['diagrams'][] = [
                 'x' => array_keys($d),
