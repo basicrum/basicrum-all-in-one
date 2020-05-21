817ca2578072d1da93bc7d4487a16b53467770a8
CodeRaiser <code.geeks1@gmail.com>
2020-05-22 00:45:16 +0300

    Revert occasionally submitted file

--- public/beacon/catcher.php
+++ public/beacon/catcher.php
@@ -8,13 +8,11 @@ $debugMode = isset($_GET['debug_mode']);
 
 // Hacking quickly to handle Cross Origin Requests
 // Better if we implement this in NIGIX level
-// $origin = !empty($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
 $origin = !empty($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
 $originHeader = 'Access-Control-Allow-Origin: '.$origin;
 
 if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
     header($originHeader);
-    header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header('Access-Control-Allow-Headers: X-Requested-With, Keep-Alive, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
     header('Access-Control-Max-Age: 86400');
