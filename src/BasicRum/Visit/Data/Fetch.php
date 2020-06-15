7f00d123ed7d890b41f5e09fbde0988804617ade
CodeRaiser <code.geeks1@gmail.com>
2020-06-15 23:49:32 +0300

    nt to rdf in sql queries and pageViewId to rumDataId

--- src/BasicRum/Visit/Data/Fetch.php
+++ src/BasicRum/Visit/Data/Fetch.php
@@ -24,17 +24,17 @@ class Fetch
     /**
      * @return mixed
      */
-    public function fetchNavTimingsInRange(int $startId, int $endId): array
+    public function fetchRumDataFlatInRange(int $startId, int $endId): array
     {
         $repository = $this->registry
             ->getRepository(RumDataFlat::class);
 
-        $query = $repository->createQueryBuilder('nt')
-            ->where("nt.rumDataId >= '".$startId."' AND nt.rumDataId <= '".$endId."'")
-            ->andWhere('nt.deviceTypeId != :deviceTypeId')
+        $query = $repository->createQueryBuilder('rdf')
+            ->where("rdf.rumDataId >= '".$startId."' AND rdf.rumDataId <= '".$endId."'")
+            ->andWhere('rdf.deviceTypeId != :deviceTypeId')
             ->setParameter('deviceTypeId', $this->filter->getBotDeviceTypeId())
-            ->select(['nt.rt_si', 'nt.createdAt', 'nt.rumDataId', 'nt.urlId'])
-            ->orderBy('nt.rumDataId', 'DESC')
+            ->select(['rdf.rt_si', 'rdf.createdAt', 'rdf.rumDataId', 'rdf.urlId'])
+            ->orderBy('rdf.rumDataId', 'DESC')
             ->getQuery();
 
         return $query->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
@@ -43,18 +43,18 @@ class Fetch
     /**
      * @return mixed
      */
-    public function fetchNavTimingsInRangeForSession(int $startId, int $endId, string $rt_si): array
+    public function fetchRumDataFlatInRangeForSession(int $startId, int $endId, string $rt_si): array
     {
         $repository = $this->registry
             ->getRepository(RumDataFlat::class);
 
-        $query = $repository->createQueryBuilder('nt')
-            ->where("nt.pageViewId >= '".$startId."' AND nt.rumDataId <= '".$endId."'")
-            ->andWhere('nt.deviceTypeId != :deviceTypeId')
-            ->andWhere('nt.rt_si = :rt_si')
+        $query = $repository->createQueryBuilder('rdf')
+            ->where("rdf.pageViewId >= '".$startId."' AND rdf.rumDataId <= '".$endId."'")
+            ->andWhere('rdf.deviceTypeId != :deviceTypeId')
+            ->andWhere('rdf.rt_si = :rt_si')
             ->setParameter('deviceTypeId', $this->filter->getBotDeviceTypeId())
             ->setParameter('rt_si', $rt_si)
-            ->select(['nt.rt_si', 'nt.createdAt', 'nt.rumDataId', 'nt.urlId'])
+            ->select(['rdf.rt_si', 'rdf.createdAt', 'rdf.rumDataId', 'rdf.urlId'])
             ->getQuery();
 
         return $query->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
@@ -86,12 +86,12 @@ class Fetch
         $repository = $this->registry
             ->getRepository(VisitsOverview::class);
 
-        $pageViewId = (int) $repository->createQueryBuilder('vo')
+        $rumDataId = (int) $repository->createQueryBuilder('vo')
             ->select('MAX(vo.lastPageViewId)')
             ->getQuery()
             ->getSingleScalarResult();
 
-        return $pageViewId;
+        return $rumDataId;
     }
 
     public function fetchPreviousSessionPageView(array $pageView): array
@@ -99,15 +99,15 @@ class Fetch
         $repository = $this->registry
             ->getRepository(RumDataFlat::class);
 
-        $pageViewId = $pageView['rumDataId'];
+        $rumDataId = $pageView['rumDataId'];
         $rt_si = $pageView['rt_si'];
 
-        $query = $repository->createQueryBuilder('nt')
-            ->where("nt.rumDataId < '".$pageViewId."'")
-            ->andWhere('nt.rt_si = :rt_si')
+        $query = $repository->createQueryBuilder('rdf')
+            ->where("rdf.rumDataId < '".$rumDataId."'")
+            ->andWhere('rdf.rt_si = :rt_si')
             ->setParameter('rt_si', $rt_si)
-            ->select(['nt.createdAt', 'nt.rumDataId', 'nt.rt_si'])
-            ->orderBy('nt.rumDataId', 'DESC')
+            ->select(['rdf.createdAt', 'rdf.rumDataId', 'rdf.rt_si'])
+            ->orderBy('rdf.rumDataId', 'DESC')
             ->setMaxResults(1)
             ->getQuery();
 
